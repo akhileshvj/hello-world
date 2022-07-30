7664325cc9cd3f4aa57705b34c68dfb91049307d
akhileshvj <akhilesh75vijay@gmail.com>
2022-07-30 18:10:34 +0530

    commit

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ for job in jobs:
     if 'few' in published_date:
         company_name = job.find('h3',class_ = 'joblist-comp-name').text.replace(' ','')
         skills = job.find('span',class_ = 'srp-skills').text.replace(' ','')
-        print(f"Company Name: {company_name.strip()} Skills: {skills.strip()} Published: {published_date.strip()}")
+        print(f"Company Name: {company_name.strip()} | Skills: {skills.strip()}  | Published: {published_date.strip()}")
 
     print('')
 
