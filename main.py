c15eeb643d937f6023d8497e2bc4a46c8da066fe
akhileshvj <akhilesh75vijay@gmail.com>
2022-07-30 18:03:06 +0530

    commit

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hello World akhilesh")
+from bs4 import BeautifulSoup
+import requests
+
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=Python&txtLocation=India').text
+# print(html_text)
+
+soup =BeautifulSoup(html_text, 'lxml')
+jobs = soup.find_all('li',class_ = 'clearfix job-bx wht-shd-bx')
+
+for job in jobs:
+    published_date = job.find('span', class_='sim-posted').text.replace(' ', '')
+    if 'few' in published_date:
+        company_name = job.find('h3',class_ = 'joblist-comp-name').text.replace(' ','')
+        skills = job.find('span',class_ = 'srp-skills').text.replace(' ','')
+
+
+        print(f'''
+        Company Name: {company_name}
+        Skills: {skills}
+        published: {published_date}
+        ''')
+
+    print('')
+
+
+
+
